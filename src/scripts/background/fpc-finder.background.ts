e6477a6fcc66f138c70d387b5de712432ea63574
Michał Czapliński <czaplinskim@icloud.com>
2023-04-26 15:40:35 +0200

    Added comments

--- src/scripts/background/fpc-finder.background.ts
+++ src/scripts/background/fpc-finder.background.ts
@@ -1,5 +1,6 @@
+// Checks langauge of EANS we are working on
 
-const findUrlLanguage = () => {
+const findUrlLanguage: Function = () => {
 
     const queryString = window.location.search
     const urlParams = new URLSearchParams(queryString)
@@ -9,12 +10,14 @@ const findUrlLanguage = () => {
 
 }
 
-export const startExtenstionFPCFinder = () => {
+
+
+export const startExtenstionFPCFinder: Function = () => {
 
 
     findUrlLanguage() 
 
-    const findProducts = () => { 
+    const findProducts : Function = () => { 
 
         var products = document.querySelectorAll(".name-inner")
         var result = Object.keys(products).map((key) => products[key].innerText)
@@ -26,17 +29,16 @@ export const startExtenstionFPCFinder = () => {
     }
 
 
-    const createBagde = (badgeName) => {
+    const createBagde = (badgeName : string) => {
 
         setTimeout(() => {
 
-            const badge = document.createElement("a")
-            const badgesStyles = 'color: black; font-weight: 500; margin: 0px 5px; font-size: 13px; cursor: pointer;'
-    
+            const badge : HTMLAnchorElement = document.createElement("a")
+            const badgesStyles : string | HTMLStyleElement = 'color: black; font-weight: 500; margin: 0px 5px; font-size: 13px; cursor: pointer;'
             badge.setAttribute('target', '_blank')
             badge.style.cssText = badgesStyles
             badge.textContent = badgeName
-            const searchTab = document.querySelector(".files-browser-bar")
+            const searchTab : Element = document.querySelector(".files-browser-bar")
             searchTab.insertAdjacentElement("beforeend", badge)
             badge.addEventListener("click", () => { createLink(findProducts()) } )
     
@@ -45,7 +47,10 @@ export const startExtenstionFPCFinder = () => {
     }
 
     createBagde('FPCs')
-    const createLink = (array) => {
+
+    // Creates a new link that will open a new page with filtered FPCs  
+
+    const createLink = (array: Array<string>) => {
 
         const myUrl = new URL("https://pg.browser.synthrone.io/search");
         myUrl.searchParams.append('type', 'pdpd')
@@ -56,13 +61,13 @@ export const startExtenstionFPCFinder = () => {
 
     }
 
-    
+    // Function that creates an array of arrays [[EAN1, FPC1], [EAN1, FPC2], [EAN2, FPC3], ...]
 
-    const cutNames = (array) => {
+    const cutNames = (array: Array<string>) => {
 
         let productsInfoArray = []
 
-        array.forEach((product, index) => {
+        array.forEach((product) => {
 
             let tempEan = Number(product.split('-')[0])
             let tempFpc = Number(product.split('-')[1])
@@ -72,7 +77,12 @@ export const startExtenstionFPCFinder = () => {
         
         })
 
+        // Sort arrays by higher FPCs
+
         productsInfoArray = productsInfoArray.sort((a, b) => { return a[1] - b[1] })
+
+        // Deletes smaller FPCs of doubled EANs
+
         productsInfoArray = productsInfoArray.reduce((a, v, i) => ({ ...a, [v[0]]: v[1]}), {}) 
 
         return productsInfoArray
