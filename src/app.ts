0a41ded1c9590ae034959fd9ae52ff237b18f649
Michał Czapliński <czaplinskim@icloud.com>
2023-04-26 14:41:06 +0200

    Last changes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import { startExtenstionFPCFinder } from "./scripts/background/fpc-finder.background.js"
-import { startExtenstionSyndigoDownloader } from "./scripts/background/syndigo-downloader.background.js";
 
 export function main() {
 
     if(window.location.href.indexOf("pdpd") != -1){ 
       startExtenstionFPCFinder();
-    } else if (window.location.href.indexOf("https://platform.syndigo.com/enhanced-content/previews/") != -1){
-      startExtenstionSyndigoDownloader();
+    } else if (window.location.href.indexOf("") != -1){
+      
     }
     
   }
